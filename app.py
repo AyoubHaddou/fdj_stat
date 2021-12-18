7290bf51078d702431fe71e4c7de1b990287d4a1
Ayoub <ayoubhaddou1@gmail.com>
2021-12-18 02:35:18 +0100

    'tris import inutile'

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from json import encoder
 import streamlit as st 
 import pandas as pd
-from streamlit import cursor 
 from pymongo import MongoClient
 from fonction import * 
 
 
 # Import de ma database sur mongodb cloud 
-# client = MongoClient('mongodb+srv://ayoub:dnU*r*kNQXMj2pv@cluster0.pblvj.mongodb.net/test')
 client = MongoClient(**st.secrets["mongo"])
 db = client.Database_scrapy.Loto2
 
