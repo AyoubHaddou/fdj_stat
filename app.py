bc7af6fe6991ac1d9c9762e01e0fba43fb5ca71e
Ayoub <ayoubhaddou1@gmail.com>
2021-12-18 13:52:59 +0100

    maj index

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ cursor = list(db.find({},{'_id':0}))
 df = pd.DataFrame(cursor)
 n = [n for n in df['Numero']]
 df2 = pd.DataFrame(n)
+df2.index += 1 
 
 # Declaration des variables et des listes
 a = st.sidebar.selectbox('Hello',['En Datafram','Les 5 numéros gagnant','Le numéro chance','Recherche'])
@@ -23,16 +24,19 @@ col1,col2,col3 = st.columns(3)
 table_chance_100 = []
 for i in range(1,11):
     table_chance_100.append({'N° chance' :  i, 'Proba en %' : (count_number_chance(df,i) / 2094 * 100)})
+df_table_chance_100 = pd.DataFrame(table_chance_100)
+df_table_chance_100.index = [i for i in range (1,11)]
 
 table_100 = []
 for i in range(1,50):
     table_100.append({'Numero' :  i, 'Proba en %' : (count_number(df,i) / 2094 * 100)})
-
+df_table_100 = pd.DataFrame(table_100)
+df_table_100.index +=1 
 num = []
 for i in range(1,50):
     num.append({'Numero :'+str(i) : count_number(df,i)})
 num_df = pd.DataFrame(num)
-num_df.index = [i for i in range (1,50)]
+num_df.index += 1 
 
 num_p = []
 for i in range(1,50):
@@ -42,27 +46,32 @@ num_p_df.index = [i for i in range (1,50)]
 num_chance = []
 for i in range(1,11):
     num_chance.append({i : count_number_chance(df,i)})
+df_num_chance = pd.DataFrame(num_chance)
+df_num_chance.index += 1
 
 num_chance_p = []
 for i in range(1,11):
     num_chance_p.append({i : count_number_chance(df,i) / 2094 *100})
+df_num_chance_p = pd.DataFrame(num_chance_p)
+df_num_chance_p.index += 1 
 
 table_chance_100_t = []
 for i in range(1,11):
     table_chance_100_t.append({'Nombre de sortie' :  count_number_chance(df,i), 'Proba en %' : (count_number_chance(df,i) / 2094 * 100)})
+df_table_chance_100_t = pd.DataFrame(table_chance_100_t)
+df_table_chance_100_t.index += 1 
 
 table_c_100 = []
 for i in range(1,50):
     table_c_100.append({'Nombre de sortie' :  count_number(df,i), 'Proba en %' : float(count_number(df,i) / 2094 * 100)})
 result = pd.DataFrame(table_c_100)
-result.index = [i for i in range(1,50)]
+result.index +=1 
 
 table_c_100_chance = []
 for i in range(1,11):
     table_c_100_chance.append({'Nombre de sortie' :  count_number_chance(df,i), 'Proba en %' : float(count_number_chance(df,i) / 2094 * 100)})
-
 result1 = pd.DataFrame(table_c_100_chance)
-result1.index = [i for i in range(1,11)]
+result1.index += 1 
 
 if a == 'En Datafram':
 
@@ -106,11 +115,11 @@ if a == 'Les 5 numéros gagnant':
 
 if a == 'Le numéro chance':
     st.subheader('Nombre de tirage pour chaque numéro chance')
-    st.bar_chart(num_chance)
+    st.bar_chart(df_num_chance)
     st.subheader('Nombre de tirage pour chacun numéro chance en pourcentage')
-    st.bar_chart(num_chance_p)
-    st.bar_chart(table_chance_100)
-    st.bar_chart(table_chance_100_t)
+    st.bar_chart(df_num_chance_p)
+    st.bar_chart(df_table_chance_100)
+    st.bar_chart(df_table_chance_100_t)
     st.line_chart(result1)
     st.area_chart(result1)
 
